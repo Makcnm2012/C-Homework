fdd85fceb7ae0f507ebe334d6142cddb6e7f2ac8
Makcnm2012 <55019301+Makcnm2012@users.noreply.github.com>
2020-04-04 17:50:40 +0300

    Update Task 2.cpp

--- Week 1/Task 2.cpp
+++ Week 1/Task 2.cpp
@@ -1,1 +1,43 @@
-#include <iostream>#include <ctime>#include <cstdlib>using namespace std;int main(int argc, char* argv[]){    srand(time(NULL));        int n = 10;    int temp = 0;        int *arr = new int [n];        for (int i = 0; i < n; i++)        arr[i] = (rand() % 10 + 1);        cout << "massive = ";        for (int i = 0; i < n; i++)       cout << arr[i] << " ";        for (int i = 0; i < n; i++){        for(int j = i + 1; j < n; j++){            if (arr[i] < arr[j]){                temp = arr[i];                arr[i] = arr[j];                arr[j] = temp;            }        }    }        cout << endl << "sorted massive = ";        for (int i = 0; i < n; i++)       cout << arr[i] << " ";        delete [] arr;     cout << endl;        return 0;}
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    srand(time(NULL));
+    
+    int n = 10;
+    int temp = 0;
+    
+    int *arr = new int [n];
+    
+    for (int i = 0; i < n; i++)
+        arr[i] = (rand() % 10 + 1);
+    
+    cout << "massive = ";
+    
+    for (int i = 0; i < n; i++)
+       cout << arr[i] << " ";
+    
+    for (int i = 0; i < n; i++){
+        for(int j = i + 1; j < n; j++){
+            if (arr[i] < arr[j]){
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    
+    cout << endl << "sorted massive = ";
+    
+    for (int i = 0; i < n; i++)
+       cout << arr[i] << " ";
+    
+    delete [] arr; 
+    cout << endl;
+   
+    return 0;
+}
