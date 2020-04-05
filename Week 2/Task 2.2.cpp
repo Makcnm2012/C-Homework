3f8017f96e6c9c5392d7a537305454b4114694a5
Makcnm2012 <makcnma2@ua.fm>
2020-04-05 19:41:12 +0300

    Create Task 2.2.cpp

---
+++ Week 2/Task 2.2.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <cmath>
+
+using namespace std;
+
+int simple (int *arr, int n) {
+    
+    bool notsimple = false; 
+    
+    for (int i = 0; i < n; i++) {
+    
+        for (int j = 2; j<=sqrt(arr[i]); j++) {
+            if (arr[i] % j == 0) {
+                notsimple = true;
+                break;
+            }
+        }    
+    
+        if (!notsimple){
+            if (arr[i] > 1) cout << arr[i] << " ";
+        } else {    
+            notsimple = false;
+        }
+    }  
+    
+    return 0;
+}
+
+int main(int argc, char* argv[])
+{
+    srand(time(NULL));
+    
+    int n = 10;
+    
+    int *arr = new int [n];
+
+    for (int i = 0; i < n; i++)
+        arr[i] = (rand() % 10 + 1);
+
+    cout << "massive = ";
+    
+    for (int i = 0; i < n; i++)
+       cout << arr[i] << " ";
+
+    cout << endl << "Simple elements: "; 
+    simple (arr, n); 
+
+    delete [] arr; 
+    cout << endl;
+   
+    return 0;
+}
