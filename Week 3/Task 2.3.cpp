0eee2f00538333d3b328c7178ebfe2234b2185be
Makcnm2012 <makcnma2@ua.fm>
2020-04-14 15:55:40 +0300

    task 3

--- Week 3/Task 2.3.cpp
+++ Week 3/Task 2.3.cpp
@@ -6,42 +6,78 @@ using namespace std;
 void view(const int*, int); //оголошення функції 
 void createVect(int*&, int); //оголошення функції 
 
+
 //функція для відображення елементів масиву 
 void view(const int* X, int size) { 
     
-    cout << "Масив: " << endl; 
+    cout << "Масив: " << endl;
     for (int i=0; i<size; i++) { 
-        cout << X[i] << '\t'; 
+        cout << X[i] << '\t';
     } 
     cout << endl << endl;
-    
 } 
 
 //функція, що заповнює масив випадковими значеннями 
 void createVect(int*&X, int size) { 
 
-    cout << "Введить елементи масиву: " << endl;
-    for(int i=0; i<size; i++) {
-        cout << "[" << i + 1 << "] -> ";
-        cin >> X[i];
-    }
+    srand((time(NULL))); 
+    for(int i=0; i<size; i++) 
+        X[i] = rand()%20-10;
 } 
+
+void SelNum (int*X, int size ){
+    
+    cout << "Непарні елементи массиву: ";
+    
+    for (int i = 0; i < size; i++) {
+        
+        if (X[i] % 2 == 0) {
+            X[i] = 0;
+        } else {
+            cout << X[i] << '\t';
+        }
+    }   
+    cout << endl;
+}  
 
 int main() { 
     
     int n; 
-    int *pA; 
+    int *A, *B, *C; 
     
     cout << "Задайте кiлькiсть елементiв -> "; 
     cin >> n; 
     
-    pA = new int[n]; //виділення пам’яті 
-    createVect(pA, n); //виклик функції створення масиву кількістю елементів n 
-    view(pA, n); // виклик функції виведення елементів масиву на екран 
-    //pA вказівник на масив, n кількість елементів 
-   
-    delete []pA; //очистка пам’яті 
-  
+    A = new int[n]; //виділення пам’яті 
+    createVect(A, n); //виклик функції створення масиву кількістю елементів n 
+    view(A, n); // виклик функції виведення елементів масиву на екран 
+    SelNum(A, n);
+    view(A, n);
+    //A вказівник на масив, n кількість елементів 
+    //Створення іншого масиву
+    
+    cout << "Задайте кiлькiсть елементiв -> "; 
+    cin >> n; 
+    
+    B = new int[n]; 
+    createVect(B, n); 
+    view(B, n); 
+    SelNum(B, n);
+    view(B, n);
+    
+    cout << "Задайте кiлькiсть елементiв -> "; 
+    cin >> n; 
+    
+    C = new int[n]; 
+    createVect(C, n); 
+    view(C, n); 
+    SelNum(C, n);
+    view(C, n);
+    
+    delete []A; //очистка пам’яті 
+    delete []B; 
+    delete []C;
+        //system("pause"); 
+    
     return 0; 
 }
-
--- Week 3/Task 3.cpp
+++ Week 3/Task 3.cpp
@@ -6,42 +6,95 @@ using namespace std;
 void view(const int*, int); //оголошення функції 
 void createVect(int*&, int); //оголошення функції 
 
+
 //функція для відображення елементів масиву 
 void view(const int* X, int size) { 
-    
-    cout << "Масив: " << endl; 
+
     for (int i=0; i<size; i++) { 
-        cout << X[i] << '\t'; 
+        cout << X[i] << '\t';
     } 
     cout << endl << endl;
-    
 } 
 
 //функція, що заповнює масив випадковими значеннями 
-void createVect(int*&X, int size) { 
+void createVect(int*&X, int size, int min, int max) { 
 
-    cout << "Введить елементи масиву: " << endl;
-    for(int i=0; i<size; i++) {
-        cout << "[" << i + 1 << "] -> ";
-        cin >> X[i];
-    }
+    srand((time(NULL))); 
+    for(int i=0; i<size; i++) 
+        X[i] = rand() % (max - min + 1) + min;
 } 
+
+void check (int*D, int*M, int*Y, int size){
+    
+    int myD = 26;
+    int myM = 7;
+    int myY = 2001;
+    
+    bool dfound = false;
+    bool mfound = false;
+    bool yfound = false;
+    
+    cout << "Date of Birth: " << myD << "." << myM << "." << myY << endl;
+    
+    for(int i = 0; i < size; i++){
+        if (D[i] == myD){
+            dfound = true;
+            break;
+        }
+    }
+    if (dfound){
+        for(int i = 0; i < size; i++){
+            if (M[i] == myM){
+                mfound = true;
+                break;
+            }
+        }
+    }
+    if (mfound){
+        for(int i = 0; i < size; i++){
+            if (Y[i] == myY){
+                yfound = true;
+                break;
+            }
+        }
+    }
+    if(yfound){
+        cout << "You are Lucky!!!";
+    } else {
+        cout << "Try Again ＞︿＜";
+    }
+    
+    
+}  
 
 int main() { 
     
-    int n; 
-    int *pA; 
+    int n = 10; 
+    int *D, *M, *Y; 
+
+    D = new int[n]; //виділення пам’яті 
+    createVect(D, n, 1, 30); //виклик функції створення масиву кількістю елементів n 
+    cout << "Day: " << endl;
+    view(D, n); // виклик функції виведення елементів масиву на екран 
+    //A вказівник на масив, n кількість елементів 
+    //Створення іншого масиву
     
-    cout << "Задайте кiлькiсть елементiв -> "; 
-    cin >> n; 
+    M = new int[n]; 
+    createVect(M, n, 1, 12); 
+    cout << "Mounth: " << endl;
+    view(M, n); 
     
-    pA = new int[n]; //виділення пам’яті 
-    createVect(pA, n); //виклик функції створення масиву кількістю елементів n 
-    view(pA, n); // виклик функції виведення елементів масиву на екран 
-    //pA вказівник на масив, n кількість елементів 
-   
-    delete []pA; //очистка пам’яті 
-  
+    Y = new int[n]; 
+    createVect(Y, n, 1999, 2003); 
+    cout << "Year: " << endl;
+    view(Y, n); 
+    
+    check(D, M, Y, n);
+    
+    delete []D; //очистка пам’яті 
+    delete []M; 
+    delete []Y;
+        //system("pause"); 
+    
     return 0; 
 }
-
